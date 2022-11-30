489f4cb7f5e7216a4fceb80c967082d7f248ff04
sharathkusuru <kusurusharathreddy1997@gmail.com>
2022-11-30 16:02:54 +0530

    calculated student grades

--- src/fundamentals/AtmExample.java
+++ src/fundamentals/AtmExample.java
@@ -19,48 +19,36 @@ public class AtmExample {
             System.out.println("Choose 4 for EXIT");
             System.out.print("Choose the operation you want to perform:");
 
-            //get choice from user
+
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
                     System.out.print("Enter money to be withdrawn:");
-
-                    //get the withdrawl money from user
                     withdraw = sc.nextInt();
-
-                    //check whether the balance is greater than or equal to the withdrawal amount
                     if (balance >= withdraw) {
                         //remove the withdrawl amount from the total balance
                         balance = balance - withdraw;
                         System.out.println("Please collect your money");
                     } else {
-                        //show custom error message
                         System.out.println("Insufficient Balance");
                     }
                     System.out.println("");
                     break;
 
                 case 2:
-
                     System.out.print("Enter money to be deposited:");
-
-                    //get deposite amount from te user
                     deposit = sc.nextInt();
-
-                    //add the deposit amount to the total balanace
                     balance = balance + deposit;
                     System.out.println("Your Money has been successfully deposited");
                     System.out.println("");
                     break;
 
                 case 3:
-                    //displaying the total balance of the user
                     System.out.println("Balance : " + balance);
                     System.out.println("");
                     break;
 
                 case 4:
-                    //exit from the menu
                     System.exit(0);
             }
         }
---
+++ src/fundamentals/loop/Grade.java
@@ -0,0 +1,74 @@
+package fundamentals.loop;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+        int count =4;
+        float totalMarks =0,percetage,average;
+        int[] studentOneGrades = {78, 89, 78, 80};
+        int[] studentTwoGrades = {78, 67, 68, 92};
+        int[] studentThreeGrades = {88, 92, 92, 94};
+
+        String[] students = {"sharath", "varun", "raghu"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Student Names: " + Arrays.toString(students));
+        System.out.print("Enter a student's name:");
+        String selectedStudentName = scanner.nextLine();
+       if (selectedStudentName.equals("sharath")){
+           System.out.println("grades is :"+Arrays.toString(studentOneGrades));
+           for(int i=1;i<count;i++){
+               totalMarks += studentOneGrades[i];
+           }
+           System.out.println("totalMaraks is:"+totalMarks);
+           percetage =(totalMarks/400)*100;
+           System.out.println("percenatge is :"+ percetage);
+
+
+       }
+       if(selectedStudentName.equals("varun")){
+           System.out.println("grade is :"+Arrays.toString(studentTwoGrades));
+           for(int i=1;i<count;i++){
+               totalMarks += studentTwoGrades[i];
+           }
+           System.out.println("totalMaraks is:"+totalMarks);
+           percetage =(totalMarks/400)*100;
+
+           System.out.println("percenatge is :"+ percetage);
+
+
+       }
+       if(selectedStudentName.equals("raghu")){
+           System.out.println("grade is :"+Arrays.toString(studentThreeGrades));
+           for(int i=1;i<count;i++){
+               totalMarks += studentThreeGrades[i];
+           }
+           System.out.println("totalMaraks is:"+totalMarks);
+           percetage =(totalMarks/400)*100;
+
+           System.out.println("percenatge is :"+ percetage);
+
+       }
+        System.out.print("The student Grade is: ");
+        percetage =(totalMarks/400)*100;
+        if(percetage>=80)
+        {
+            System.out.print("A");
+        }
+        else if(percetage>=60 && percetage<80)
+        {
+            System.out.print("B");
+        }
+        else if(percetage>=40 && percetage<60)
+        {
+            System.out.print("C");
+        }
+        else
+        {
+            System.out.print("D");
+        }
+
+    }
+}
